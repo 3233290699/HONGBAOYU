e452dd866f3719a0a84320f6f20a7290e64825cb
tate <3233290699@qq.com>
2018-10-01 19:41:38 +0800

    hongbaoyu

---
+++ resprosive nav/css/custom.js
@@ -0,0 +1,20 @@
+$(function() {
+		// $.get('pages/home.html', function(data) {
+		// 	$('#ajax .content').html(data);
+		// })
+		$('#home').click(function () {
+			$.get('pages/home.html', function(data) {
+				$('#ajax .content').html(data);
+			})
+		})
+		$('#about').click(function () {
+			$.get('pages/about.html', function(data) {
+				$('#ajax .content').html(data);
+			})
+		})
+		$('#contact').click(function () {
+			$.get('pages/contact.html', function(data) {
+				$('#ajax .content').html(data);
+			})
+		})
+	});
---
+++ welcome.php
@@ -0,0 +1,12 @@
+欢迎<?php echo $_POST["fname"]; ?>!<br>
+你的年龄是 <?php echo $_POST["age"]; ?>  岁。
+<?php
+ //$test = '好好学习天天向上';
+ //echo $test;
+ //echo "{$name}好好学习天天向上";
+ 
+ //echo php_version;
+ echo PHP_VERSION;
+ 
+ 
+ ?>
